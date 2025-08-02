053a10db219d42a2bfcecf2c8781f17fddedce75
Lance <lancehemphill2000@gmail.com>
2025-08-02 13:06:18 -0400

    improved jsons and added addtional info, created jsons that contain summaries and links of topics

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { acidjson } from "./data/flashcards/ACID.json"
+
+const ACID = acidjson;
+
+
+export const CardCategories = {
+    getCategories
+}
